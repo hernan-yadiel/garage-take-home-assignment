eea2437206567e7aebc6575dd4388ba6c4419ad9
Yadiel Vélez <16180439+hernan-yadiel@users.noreply.github.com>
2025-01-20 02:04:25 -0500

    extract inline icons into components

--- src/app/listing/[listingId]/_components/request-call-card.tsx
+++ src/app/listing/[listingId]/_components/request-call-card.tsx
@@ -1,5 +1,7 @@
-import { cn } from '@/lib/utils'
 import type { ReactElement } from 'react'
+import CalendarIcon from '@/components/icons/calendar-icon'
+import UsersIcon from '@/components/icons/users-icon'
+import { cn } from '@/lib/utils'
 
 interface Props {
     className?: string
@@ -25,41 +27,13 @@ export default function RequestCallCard({ className }: Props): ReactElement {
                     </p>
                     <div className="w-full">
                         <button className="transition-hoseflow flex w-full cursor-pointer items-center justify-center rounded-2xl border border-neutral-200 bg-white px-3.5 py-2 text-base/normal font-medium text-neutral-900 hover:border-neutral-300 hover:bg-neutral-50 active:scale-[0.99]">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                aria-hidden="true"
-                                data-slot="icon"
-                                className="mr-2 h-5 w-5"
-                            >
-                                <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"></path>
-                                <path
-                                    fillRule="evenodd"
-                                    d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
+                            <CalendarIcon className="mr-2 h-5 w-5" />
                             Schedule video call
                         </button>
                     </div>
                 </div>
                 <div className="flex items-center justify-center space-x-2 px-6 py-3">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        aria-hidden="true"
-                        data-slot="icon"
-                        className="h-5 w-5 fill-white/75"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z"
-                            clipRule="evenodd"
-                        ></path>
-                        <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z"></path>
-                    </svg>
+                    <UsersIcon className="h-5 w-5 fill-white/75" />
                     <p className="text-sm/normal font-light tracking-wide text-black text-white/75">
                         Speak directly with the seller
                     </p>
